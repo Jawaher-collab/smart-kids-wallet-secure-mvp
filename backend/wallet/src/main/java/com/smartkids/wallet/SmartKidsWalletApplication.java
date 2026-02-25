34becbf411f639cb45310782d451e1f9c1d3e358
Jawaher M Al Ghamdi <jawahermalghamdi@MacBookPro.lan>
2026-02-25 15:10:35 -0500

    Add Spring Boot backend

---
+++ backend/wallet/src/main/java/com/smartkids/wallet/SmartKidsWalletApplication.java
@@ -0,0 +1,13 @@
+package com.smartkids.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKidsWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKidsWalletApplication.class, args);
+	}
+
+}
